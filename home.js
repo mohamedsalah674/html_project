5cbbd2c15bd87fd3a0109979742fd151a09f6432
mohamedsalah674 <66376551+mohamedsalah674@users.noreply.github.com>
2020-06-04 02:16:19 +0200

    Update home.js

--- home.js
+++ home.js
@@ -23,6 +23,32 @@ alert("First name must contain only letters,underscore and numbers ");
          return false;
     }
 
+for(var i=0;i<=8;i++){
+if (form.password1.value[i]==" ")
+		{
+alert("space is not allowed in password");
+form.password1.focus();
+        
+		return false;
+		}
+	else{continue;}
+	}
+	
+
+if(form.password1.value[0]>= 'A' && form.password1.value[0] <= 'Z' )	
+{	
+	          i=0;
+               
+}	
+else{         
+			  alert("first char must be capital letter");
+              form.password1.focus(); 
+              return false;
+	}
+	
+
+
+
 if (form.password1.value != "" && form.password1.value == form.password2.value) {
 if (!pw(form.password1.value)) {
  alert("The password you have entered is not valid! , password must consist of only 8 characters including capital and small case and digits");
@@ -37,7 +63,7 @@ alert("Password and confirm passsword are not identical");
 form.password1.focus();
         return false;
     }
-    return alert("Hello you chose your right place");
+    return alert("Hello your chose your right place");
 
 
 
